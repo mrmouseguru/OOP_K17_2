b4bf8d65bbba29612356de9aaa7091974fc6d07f
CGM <mrmouse.guru@gmail.com>
2024-12-05 10:53:14 +0700

    update source code buoi 13 v3

--- src/buoi13/VitApp.java
+++ src/buoi13/VitApp.java
@@ -5,15 +5,16 @@ public class VitApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("So doi tuong vit: " + Vit.getCount());;
-		
+		Vit vit3 = null;
 		Vit vit1 = new Vit();
-		
+		vit3 = vit1;// 1 đối tượng vịt được điểu khiển bởi
+					//02 biến tham chiếu đối tượng
+		vit1 = null;//còn remote vit3 điều khiển object vit
 		//keu() của đối tượng vịt phải
 		//thông qua biến tham chiếu object (Remote)
-		vit1.keu();
-		
-	
-		
+		vit3.keu();
+		vit3 = null;//không còn remote nào điều khiển object
+				
 		System.out.println("So doi tuong vit: " + Vit.getCount());;
 		Vit vit2 = new Vit();
 		System.out.println("So doi tuong vit: " + Vit.getCount());;
